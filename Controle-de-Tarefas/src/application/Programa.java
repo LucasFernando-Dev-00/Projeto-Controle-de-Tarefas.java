c6ee4d941a635cd44b1e78cd04e0fbaf57ba5ea0
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-14 18:19:03 -0300

    Adicionei a ultima atualização

--- Controle-de-Tarefas/src/application/Programa.java
+++ Controle-de-Tarefas/src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import repository.DbBanco;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 import java.util.Locale;
 
@@ -17,6 +19,10 @@ public class Programa {
 
         conect1.DBConection();
 
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        //Preciso instaciar a Classe Tarefa com o banco de Dado e fazer com que a mesma tabela que aparece lá apareça no terminal Java
 
     }
 }
