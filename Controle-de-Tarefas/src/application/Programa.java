eef810521cc2e0267e4231fdbc149ce705620702
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-14 18:01:18 -0300

    Adicionei a conexão com banco de dados

--- Controle-de-Tarefas/src/application/Programa.java
+++ Controle-de-Tarefas/src/application/Programa.java
@@ -1,14 +1,22 @@
 package application;
 
+import repository.DbBanco;
+
 import java.util.Scanner;
 import java.util.Locale;
+
+import repository.DbBanco;
 
 public class Programa {
     public static void main(String[]main) {
        Locale.setDefault(Locale.US);
        Scanner sc = new Scanner(System.in);
 
-       
+        DbBanco conect = new DbBanco();
+        DbBanco conect1 = conect;
+
+        conect1.DBConection();
+
 
     }
 }
---
+++ Controle-de-Tarefas/src/repository/DbBanco.java
@@ -0,0 +1,59 @@
+package repository;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
+public class DbBanco {
+
+    private String url = "jdbc:mysql://localhost:3306/tarefas";
+    private String user = "root";
+    private String password = "01032008";
+
+    public DbBanco() {
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    //private PreparedStatement st = null;
+    //private ResultSet rs = null;
+
+public void DBConection() {
+
+    try {
+        Connection conn = DriverManager.getConnection(url, user, password);
+        System.out.println("Conectado com sucesso!");
+
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+}
+
+
+}
