15680bb06b71c263105e63f3e89ee3c6d9c80533
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-14 17:36:36 -0300

    Adicionado todos os pacotes

---
+++ Controle-de-Tarefas/src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Programa {
+    public static void main(String[]main) {
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+
+       
+
+    }
+}
---
+++ Controle-de-Tarefas/src/entities/Tarefa.java
@@ -0,0 +1,55 @@
+package entities;
+
+import java.util.Date;
+
+public class Tarefa {
+
+    private Integer id;
+    private String titulo;
+    private String status;
+    private Date data;
+
+    Tarefa() {
+
+    }
+
+    Tarefa(Integer id, String titulo, String status, Date data) {
+
+        this.id = id;
+        this.titulo = titulo;
+        this.status = status;
+        this.data = data;
+        }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
